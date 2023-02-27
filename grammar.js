be29a79ac6a03a783319065b8c720197e8684bba
Clément Delafargue <clement@delafargue.name>
2023-02-27 16:44:29 +0100

    Complete datalog support

--- grammar.js
+++ grammar.js
@@ -6,73 +6,99 @@ module.exports = grammar({
     [ $.term, $.fact_term ],
     [ $.term, $.set_term ],
     [ $.set_term, $.fact_term ],
+    [ $.unary_op_expression, $.binary_op_expression],
   ],
 
   rules: {
-
-   source_file: $ => repeat(choice($.authorizer_element)),
-   authorizer_element: $ => seq(
-                             choice($.rule, $.fact, $.policy, $.check),
-                             optional($.sp),
-                             ";",
-                             optional($.sp)
-                           ),
+    source_file: $ => seq(
+      optional($.comment),
+      optional(seq($.origin_clause, ";")),
+      repeat(prec(1, choice($.authorizer_element, $.comment)))
+    ),
+    
+   authorizer_element: $ => seq(choice($.rule, $.fact, $.policy, $.check), ";"),
   rule: $ => seq(
       $.predicate,
-      optional($.sp),
       "<-",
-      optional($.sp),
       $.rule_body
     ),
   check: $ => seq(
        choice("check if", "check all"),
-       optional($.sp),
        $.rule_body,
        repeat(seq(
-        "or", $.sp,
+        "or",
         $.rule_body
       ))
     ),
   policy: $ => seq(
        choice("allow if", "deny if"),
-       optional($.sp),
        $.rule_body,
        repeat(seq(
-        "or", $.sp,
+        "or",
         $.rule_body
        ))
   ),
   rule_body: $ => seq(
-      $.predicate,
+      choice($.predicate, $.expression),
       repeat(seq(
         ",",
-        optional($.sp),
         choice($.predicate, $.expression)
-      ))
+      )),
+      optional($.origin_clause)
     ),
   predicate: $ => seq(
                     $.nname,
                     "(",
-                    optional($.sp),
                     $.term,
-                    repeat(seq(",", optional($.sp), $.term)),
-                    optional($.sp),
+                    repeat(seq(",", $.term)),
                     ")"
                   ),
   fact: $ => seq(
                $.nname,
                "(",
-               optional($.sp),
                $.fact_term,
-               repeat(seq(",", optional($.sp), $.fact_term)),
-               optional($.sp),
+               repeat(seq(",", $.fact_term)),
                ")"
              ),
   expression: $ =>
-      $.term,
-  term: $ => choice($.boolean, $.bytes, $.number, $.date, $.string, $.variable, $.set),
-  fact_term: $ => choice($.boolean, $.bytes, $.number, $.date, $.string, $.set),
-  set_term: $ => choice($.boolean, $.bytes, $.number, $.date, $.string),
+    choice(
+      $.parens,
+      $.methods,
+      $.unary_op_expression,
+      $.binary_op_expression,
+      $.term
+      // todo methods
+    ),
+  parens: $ =>
+    prec(10, seq("(", $.expression, ")")),
+  methods: $ =>
+    prec(9, seq(
+      $.expression,
+      ".",
+      $.nname,
+      "(",
+      repeat($.expression),
+      ")"
+    )),
+  unary_op_expression: $ =>
+    prec(8, seq("!", $.expression)),
+  binary_op_expression: $ => choice(
+      prec.left(7, seq($.expression, "*", $.expression)),
+      prec.left(7, seq($.expression, "/", $.expression)),
+      prec.left(6, seq($.expression, "+", $.expression)),
+      prec.left(6, seq($.expression, "-", $.expression)),
+      prec.left(5, seq($.expression, "&", $.expression)),
+      prec.left(4, seq($.expression, "|", $.expression)),
+      prec.left(3, seq($.expression, "^", $.expression)),
+      // todo comparison operators are NOT associative, but not marking them as such
+      // generates an ambiguity
+      prec.left(2, seq($.expression, choice(">", "<", ">=", "<=", "==", "!="), $.expression)),
+      prec.left(1, seq($.expression, "&&", $.expression)),
+      prec.left(0, seq($.expression, "||", $.expression))
+    ),
+  term: $ => choice($.param, $.boolean, $.bytes, $.number, $.date, $.string, $.variable, $.set),
+  fact_term: $ => choice($.param, $.boolean, $.bytes, $.number, $.date, $.string, $.set),
+  set_term: $ => choice($.param, $.boolean, $.bytes, $.number, $.date, $.string),
   boolean: $ => choice("true", "false"),
   bytes: $ => token(seq("hex:", optional(repeat1(/[0-9a-f]{2}/)))),
   number: $ => token(seq(optional("-"), repeat1(/[0-9]/))),
@@ -85,21 +111,35 @@ module.exports = grammar({
     "\""
   )),
   set: $ => seq("[",
-                optional($.sp),
                 optional(seq(
                   $.set_term,
-                  optional($.sp),
                   repeat(seq(
                     ",",
-                    optional($.sp),
                     optional($.set_term)
                   )),
                 )),
                 "]"
             ),
-  sp: $ => repeat1(choice(" ", "\t", "\n")),
   nname: $ => seq(/[a-zA-Z]/, repeat(/[a-zA-Z0-9_:]/)),
   variable: $ => seq("$", repeat1(/[a-zA-Z0-9_:]/)),
-  }
+  param: $ => seq("{", $.nname, "}"),
+  origin_clause: $ =>
+    seq("trusting", $.origin_element, repeat(seq(",", $.origin_element))),
+  origin_element: $ =>
+    choice("previous", "authority", seq("ed25519/", repeat1(/[0-9a-f]{2}/))),
+  comment: $ => choice(
+    $.line_comment,
+    $.block_comment
+  ),
+  // copied from tree-sitter-dhall
+  line_comment: $ =>
+    seq("//", repeat($._not_end_of_line), "\n"),
+  block_comment: $ =>
+    seq("/*", $._block_comment_continue),
+  _not_end_of_line: $ => /[\x20-\uD7FF]/,
+  _block_comment_chunk: $ =>
+    choice($.block_comment, /[\x20-\uD7FF]/),
+  _block_comment_continue: $ =>
+    choice("*/", seq($._block_comment_chunk, $._block_comment_continue)),  }
 });
 
